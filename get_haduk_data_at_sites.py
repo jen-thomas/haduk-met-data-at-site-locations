630832663f79563d0b9a4882edd290d878d91886
Jen Thomas <jen@falciot.net>
2024-01-09 10:53:33 +0000

    Explore NetCDF data and structure.

--- get_haduk_data_at_sites.py
+++ get_haduk_data_at_sites.py
@@ -27,6 +27,25 @@ def load_data_into_iris(data_directory, file_list):
     return cubes
 
 
+def explore_netcdf_file(cube):
+    print("Cube name:", cube.standard_name)
+    print("Cube units:", cube.units)
+    print("Cube shape:", cube.shape)
+    print("Cube dimensions:", cube.ndim)
+    print("Cube details:", cube)
+
+
+def check_coordinate_names_units(cube):
+    cube_latitude = cube.coord("latitude")
+    print(f"Latitude var name: {cube_latitude.standard_name}; Units: {cube_latitude.units} ", )
+
+    cube_longitude = cube.coord("longitude")
+    print(f"Longitude var name: {cube_longitude.standard_name}; Units: {cube_longitude.units} ", )
+
+    cube_time = cube.coord("time")
+    print(f"Time var name: {cube_time.standard_name}; Units: {cube_time.units} ", )
+
+
 if __name__ == "__main__":
 
     met_data_dir = "ceda_data"
@@ -34,4 +53,6 @@ if __name__ == "__main__":
     nc_file_list = list_files(met_data_dir)
     cubes = load_data_into_iris(met_data_dir, nc_file_list)
 
-    print(cubes)
+    for cube in cubes[0:3]:
+        explore_netcdf_file(cube)
+        check_coordinate_names_units(cube)
